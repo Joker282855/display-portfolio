3ea642f1ae10f4099ece424430f893136a2bb4d3
unknown <jtjackson325@gmail.com>
2021-10-07 00:17:51 -0600

    got the three links put in at the footer section

--- src/App.js
+++ src/App.js
@@ -10,8 +10,11 @@ function App() {
       <footer>
         <div>
           <a href="https://github.com/Joker282855">Github</a>
+          <br></br>
           <a href="https://www.linkedin.com/feed/">LinkedIn</a>
+          <br></br>
           <a href="https://twitter.com/home">Twitter</a>
+          <br></br>
         </div>
       </footer>
     </div>
