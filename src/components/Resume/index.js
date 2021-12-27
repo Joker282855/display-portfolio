dcbb83852365f89a8b5ab62ba95cb42f7d474821
unknown <jtjackson325@gmail.com>
2021-12-26 20:59:04 -0700

    got the majority of the webpage finished

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -5,26 +5,29 @@ function Resume() {
         <section className="resume">
             <div>
                 <h2>Reusme Link</h2>
+                <br></br>
                 <a href="https://docs.google.com/document/d/1FDlKcgLR8NDt8x01yYhj_GmXnxWnFj9QAAbUD9ybGwo/edit">Link To Resume</a>
             </div>
-            <div>
+            <div className="hire">
                 <h2>Proficency List:</h2>
+                <br></br>
                 <ul>
                     <li>
                         Node.js
                     </li>
+                    <br></br>
                     <li>
                         MSQL
                     </li>
-                    <li>
-                        Mongoose
-                    </li>
+                    <br></br>
                     <li>
                         CSS
                     </li>
+                    <br></br>
                     <li>
                         Sequelize
                     </li>
+                    <br></br>
                     <li>
                         Javascript
                     </li>
