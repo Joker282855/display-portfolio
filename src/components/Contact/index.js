27dafd5300a83be26f7f5b15a241dd0c161e5b50
unknown <jtjackson325@gmail.com>
2021-12-26 18:08:57 -0700

    got the actual stuff for the contact part more reflective of whats required to get hired

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,68 +1,28 @@
-import { validateEmail } from '../../utils/helpers';
-import React,  { useState } from 'react';
+import React from 'react';
 
-
-function ContactForm() {
-    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
-    
-    const [errorMessage, setErrorMessage] = useState('');
-    const { name, email, message } = formState;
-
-    function handleChange(e) {
-        if (e.target.name === 'email') {
-            const isValid = validateEmail(e.target.value);
-            console.log(isValid);
-            // isValid conditional statement
-            if (!isValid) {
-                setErrorMessage('Your email is invalid.');
-            } else {
-                setErrorMessage('');
-            }
-        } else {
-            if (e.target.value.length) {
-                setErrorMessage(`${e.target.name} is required.`);
-            } else {
-                setErrorMessage('');
-            }
-        }
-
-        if (!errorMessage) {
-            setFormState({...formState, [e.target.name]: e.target.value });
-        }
-    }
-
-    console.log(formState);
-
-    function handleSubmit(e) {
-        e.preventDefault();
-        console.log(formState);
-    }
-
+function Contact() {
     return (
-        <section className="my-5">
-            <h1>Contact Me</h1>
-            <form id="contact-form" onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="name">Name:</label>
-                    <input type="text" defaultValue={name} onChange={handleChange} name="name" />
-                </div>
-                <div>
-                    <label htmlFor="email">Email Address:</label>
-                    <input type="email" defaultValue={email} name="email" onChange={handleChange} />
-                </div>
-                <div>
-                    <label htmlFor="message">Message:</label>
-                    <textarea name="message" defaultValue={message} onChange={handleChange} rows="5"  />
-                </div>
-                {errorMessage && (
-                    <div>
-                        <p className="error-text">{errorMessage}</p>
-                    </div>
-                )}
-                <button type="submit">Submit</button>
-            </form>
+        <section id="contact-information" className="contact">
+            <h2>Contact Information</h2>
+            <div className="list">
+                <ul>
+                    <li>
+                        Phone Number: 801-400-8251
+                    </li>
+                    <li>
+                        Address 1: 4515 Brookshire Dr. Provo, UT, 84604
+                    </li>
+                    <li>
+                        Address 2: 835 North 50 East Avenues Apartments 220, Provo, UT, 84604
+                    </li>
+                    <li>
+                        Email: jtjackson325@gmail.com
+                    </li>
+                </ul>
+            </div>
         </section>
     );
+
 }
 
-export default ContactForm;
+export default Contact;
