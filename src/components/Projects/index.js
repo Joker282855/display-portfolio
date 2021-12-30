4471ec562b8a1082a0dfe6951482b75cdbc03eaa
unknown <jtjackson325@gmail.com>
2021-12-30 10:47:53 -0700

    added the details for project one

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -6,7 +6,21 @@ function Projects() {
             <h2>Project 1</h2>
                 <ul>
                     <li>
-                        Problem Solved: This application gives user a website 
+                        Problem Addressed: There are times where people can not remeber the artists of a particular song they are trying to remember. Our applications allows users to search
+                        for a songs made by any artists which helps them remeber songs they can't remeber. 
+                    </li>
+                    <li>
+                        Concepts/Technologies Used: The initial concept was to give allow our user to find songs that fit their mood. We were on our way to accomplishing this goal with our
+                        application before complications made us change course. The complications that arose were having two group members not being able to push commits to our github 
+                        repostiory and one group member not having time to work on the website. We decided to have our website be a music finder and random music story generator due to
+                        these complications. The design concept we used was user responsive design. 
+                        Technologies Used: Visual Studio Code, Github, HTML, CSS, and Fetch Requests 
+                    </li>
+                </ul>
+            <h2>Project 2</h2>
+                <ul>
+                    <li>
+                        Problem Addressed: Teachers have to post mateirals for their students to use in multiple spots on their schools
                     </li>
                 </ul>
         </section>
